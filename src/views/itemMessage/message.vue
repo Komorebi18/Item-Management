c06917fdd1c52e8f43e271a3169c262a638c076c
Komorebi18 <2545339940@qq.com>
2024-01-18 15:04:17 +0800

    feat: 新增列表样式模板

--- src/views/itemMessage/message.vue
+++ src/views/itemMessage/message.vue
@@ -1,51 +1,72 @@
 <template>
-  <el-table :data="tableData" style="width: 100%">
-    <el-table-column type="expand">
-      <template #default="props">
-        <el-form label-position="left" inline>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-          <el-form-item label="State">
-            <span>{{ props.row.state }}</span>
-          </el-form-item>
-        </el-form>
-    </template>
-    </el-table-column>
-    <el-table-column label="Date" prop="date" />
-    <el-table-column label="Name" prop="name" />
-  </el-table>
+  <div>
+    <el-table :data="tableData" style="width: 100%">
+      <el-table-column type="expand">
+        <template #default="props">
+          <el-form label-position="left" inline style="padding-left: 50px">
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-form-item label="State:">
+                  <span>{{ props.row.state }}</span>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="City:">
+                  <span>{{ props.row.city }}</span>
+                </el-form-item>
+              </el-col>
+            </el-row>
+
+            <el-row :gutter="20">
+              <el-col :span="12">
+                <el-form-item label="Address:">
+                  <span>{{ props.row.address }}</span>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="Zip:">
+                  <span>{{ props.row.zip }}</span>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </template>
+      </el-table-column>
+      <el-table-column label="Date" prop="date" />
+      <el-table-column label="Name" prop="name" />
+      <el-table-column fixed="right" label="Operations" width="120">
+        <template #default="props">
+          <el-button
+            link
+            type="primary"
+            size="small"
+            @click="viewItemLogo(props.$index)"
+          >
+            <!-- props.$index: 序号, props.row：当前行的信息 -->
+            查看日志
+          </el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <pagination :total="100" :page="1" :limit="20" />
+    <!-- 日志弹出框 -->
+    <el-dialog v-model="visible" :show-close="false">
+      <div class="my-header">
+        <h4>日志弹窗</h4>
+        <el-button type="danger" @click="visible = false">
+          <el-icon></el-icon>
+          Close
+        </el-button>
+      </div>
+      <div>日志具体内容</div>
+    </el-dialog>
+  </div>
 </template>
 
-<!-- <p>State: {{ scope.row.state }}</p>
-            <p>City: {{ scope.row.city }}</p>
-            <p>Address: {{ scope.row.address }}</p>
-            <p>Zip: {{ scope.row.zip }}</p> -->
 <script lang="ts" setup>
+import pagination from "@/components/Pagination/index.vue";
+// 控制日志弹窗
+let visible = ref(false);
 const tableData = [
   {
     date: "2016-05-03",
@@ -72,6 +93,19 @@ const tableData = [
     ]
   }
 ];
+const viewItemLogo = (index: number) => {
+  // 打开日志弹窗
+  visible.value = true;
+  // 拿到对应物品的id
+  console.log(index);
+  // 利用物品id发送请求获取对应日志
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.my-header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+}
+</style>
--- src/views/itemMessage/relationships.vue
+++ src/views/itemMessage/relationships.vue
@@ -1,4 +1,43 @@
 <template>
+  <div>
+    <el-table :data="tableData" :border="true" :header-cell-style="headerCellStyle" :cell-style="cellStyle">
+      <!-- 表格列定义 -->
+    </el-table>
+    <el-pagination
+      :current-page="currentPage"
+      :total="tableData.length"
+      @current-change="handleCurrentChange"
+    ></el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      tableData: [], // 表格数据源
+      currentPage: 1, // 当前页码
+      pageSize: 10, // 每页显示条数
+      headerCellStyle: {
+        backgroundColor: '#f5f7fa',
+        color: '#333',
+        fontWeight: 'bold'
+      },
+      cellStyle: {
+        backgroundColor: '#fff',
+        color: '#666'
+      }
+    };
+  },
+  methods: {
+    handleCurrentChange(val) {
+      this.currentPage = val;
+    }
+  }
+};
+</script>
+
+<!-- <template>
   <div class="">层级关系
   </div>
 </template>
@@ -8,3 +47,4 @@
 <style lang="scss" scoped>
 
 </style>
+ -->
