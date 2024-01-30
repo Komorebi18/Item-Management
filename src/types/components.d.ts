0d485d409ca20260c8228dcae234459042265785
Komorebi18 <2545339940@qq.com>
2024-01-30 16:44:28 +0800

    fix: 修复图片路径出错

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -18,6 +18,8 @@ declare module '@vue/runtime-core' {
     ElDropdown: typeof import('element-plus/es')['ElDropdown']
     ElDropdownItem: typeof import('element-plus/es')['ElDropdownItem']
     ElDropdownMenu: typeof import('element-plus/es')['ElDropdownMenu']
+    ElForm: typeof import('element-plus/es')['ElForm']
+    ElFormItem: typeof import('element-plus/es')['ElFormItem']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElImage: typeof import('element-plus/es')['ElImage']
     ElInput: typeof import('element-plus/es')['ElInput']
